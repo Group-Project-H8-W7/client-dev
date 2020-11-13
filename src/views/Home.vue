2edad21e2acaa6fd8d531bfbb94c68dd9fa87d77
Deo Mareza <deomareza@gmail.com>
2020-11-13 14:39:07 +0700

    tertinggal

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,7 +7,6 @@
       :key="user.id"
     >{{ user.name }}</div>
     <b-button @click="start">countdown</b-button>
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
